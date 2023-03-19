b8748b689dceba37fd7137924e6cb93c7e0741f6
henrique11varela <henrique11varela@gmail.com>
2023-03-19 22:29:32 +0000

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#define WIFI_Kit_32
+#include "heltec.h"
+
+SSD1306Wire factory_display(0x3c, SDA_OLED, SCL_OLED, RST_OLED, GEOMETRY_128_64);
+
+void setup() {
+  // Serial monitor speed = 115200
+  pinMode(LED_BUILTIN, OUTPUT); //Built in LED setup
+  factory_display.init();
+}
+
+void loop() {
+
+}
